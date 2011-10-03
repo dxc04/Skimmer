dc74c93272073650aaf08fecc21bba447ebf7dda
Sandae Macalalag <sandaemc@gmail.com>
2011-10-03 17:09:08 +0800

    Saving private ryan!

---
+++ bin/skimmer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import pickle
+
+def init():
+	dict = {}
+	for dirname, dirnames, filenames in os.walk('.'):
+		for filename in filenames:
+			dict[filename] = os.path.join(dirname, filename)
+	
+	return dict
+
+def write_all(list):
+	output = open('data.pkl', 'wb')
+	pickle.dump(list, output)
+	output.close()
+	
+def read_all():
+	data_file = open('data.pkl', 'rb')
+	data = pickle.load(data_file)
+	data_file.close()
+
+	return data
+
+def get_file(filename):
+	return read_all()[filename]
+
+filename = sys.argv[1].strip()
+if (filename == 'init_repo'):
+	write_all(init())
+else:
+	os.system('vim ' + get_file(filename))
